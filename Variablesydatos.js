eb90d09474e7a60be9c87d6cfb89e3585b77e950
linavela04 <linavm200111@gmail.com>
2025-07-10 19:19:35 -0500

    Aquí agrego ejercicio 1 de variables y tipos de datos

---
+++ Variablesydatos.js
@@ -0,0 +1,9 @@
+// Ejercicio 1: Variables y Tipos de Datos
+
+let nombre = "Lina";
+let edad = 22;
+let esEstudiante = true;
+
+console.log("Nombre:", nombre);
+console.log("Edad:", edad);
+console.log("¿Es estudiante?", esEstudiante);
