2a3d07c03dcf0c507a9cc714919a4275e233ffc2
linavela04 <linavm200111@gmail.com>
2025-07-13 23:14:53 -0500

    Punto 1 completo y archivo para punto 2

--- Variablesydatos.js
+++ Variablesydatos.js
@@ -1,9 +1,19 @@
 // Ejercicio 1: Variables y Tipos de Datos
 
 let nombre = "Lina";
-let edad = 22;
+let edad = 24;
 let esEstudiante = true;
 
 console.log("Nombre:", nombre);
 console.log("Edad:", edad);
 console.log("¿Es estudiante?", esEstudiante);
+
+
+
+//Ejercicio 2
+console.log("Ejercicio 2");
+let sinValor;
+let valorNulo = null;
+
+console.log(sinValor);
+console.log(valorNulo);
